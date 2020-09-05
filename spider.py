91d203ff094460aa0f26eac4caefa5ff0e8ae00d
Robinlin <robinlin163@163.com>
2020-09-05 17:48:41 +0800

    修改爬虫功能

--- spider.py
+++ spider.py
@@ -4,20 +4,22 @@ import json
 import argparse
 import os
 import time
+from tqdm import tqdm
 from utils import getcomment, getlist
+from utils import spider_one_page
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="T大树洞爬虫")
-    parser.add_argument("--start", help="爬取帖子索引最大值，若不指定，默认为最新帖子的id",
-                        type=int)
-    parser.add_argument("--end", help=("爬取帖子索引最小值"
-                                       "若不指定，程序会自动检测路径中索引最大的id"),
-                        type=int)
+    parser = argparse.ArgumentParser(description="T大树洞爬虫, 程序会自动爬取帖子以及对应评论内容")
+    # parser.add_argument("--start", help="爬取帖子索引最大值，若不指定，默认为最新帖子的id",
+    #                     type=int)
+    # parser.add_argument("--end", help=("爬取帖子索引最小值"
+    #                                    "若不指定，程序会自动检测路径中索引最大的id"),
+    #                     type=int)
     parser.add_argument("--config", help="爬虫配置文件路径",
                         default="./config.json")
     parser.add_argument("--store-path", help="数据存储目录",
-                        default="./data")
+                        default="./posts")
     parser.add_argument("--sleep", help="爬取一次页面后休眠时间/s", type=int, default=20)
     args = parser.parse_args()
 
@@ -28,61 +30,55 @@ if __name__ == "__main__":
 
     # API
     getone_url = (f"{config['api_url']}&action=getone&"
-                  f"user_token={config['user_token']}")
+                f"user_token={config['user_token']}")
     getcomment_url = (f"{config['api_url']}&action=getcomment"
-                      f"&user_token={config['user_token']}")
+                    f"&user_token={config['user_token']}")
     getlist_url = (f"{config['api_url']}&action=getlist"
-                   f"&user_token={config['user_token']}")
-    end = args.end
+                f"&user_token={config['user_token']}")
     store_path = args.store_path
     sleep_time = args.sleep
 
+    # 获取最新帖子pid
     max_pid = 0
-    start = args.start
     page = getlist(1, getlist_url)
     page = json.loads(page, encoding='utf-8')
-    max_pid = max(map(lambda x: int(x), page['data'].keys()))
-    if start is None:
-        start = max_pid
+    max_pid = max(map(lambda x: int(x['pid']), page['data']))
 
     # 确认爬取帖子索引最小值
-    if end is None:
-        filenames = os.listdir(store_path)
-        path_exist = os.path.exists(store_path)
-        if path_exist and len(filenames) != 0:
-            end = max(map(lambda x: int(x.split('.')[0]), filenames))
-        elif not path_exist:
-            os.makedirs(store_path)
-            end = 0
-        else:
-            end = 0
+    # if end is None:
+    #     filenames = os.listdir(store_path)
+    #     path_exist = os.path.exists(store_path)
+    #     if path_exist and len(filenames) != 0:
+    #         end = max(map(lambda x: int(x.split('.')[0]), filenames))
+    #     elif not path_exist:
+    #         os.makedirs(store_path)
+    #         end = 0
+    #     else:
+    #         end = 0
 
-    # spider
+    # 获取已爬取的帖子
+    if os.path.exists(store_path):
+        pid_list = map(lambda x: int(x.split('.')[0]),
+                       os.listdir(store_path))
+    else:
+        os.makedirs(store_path)
+        pid_list = []
+
+    # 爬取页面
     p = 1
-    reach_end = False
-    while not reach_end:
-
-        start = time.time()
+    page_num = max_pid // 30 + 1
+    for p in tqdm(range(1, page_num + 1)):
+        start_t = time.time()
         page = getlist(p, getlist_url)
 
-        page = json.loads(page, encoding='utf-8')
-        page_data = page['data']
-        for post in page_data:
-            pid = post['pid']
-            if pid > end:
-                comment = getcomment(pid, getcomment_url)
-                post['comment'] = json.loads(comment, encoding='utf-8')
-                dst = os.path.join(store_path, f"{pid}.json")
-                with open(dst, 'w', encoding='utf-8') as f:
-                    f.write(json.dumps(post, ensure_ascii=False))
-            elif pid <= end and p == 1:
-                pass
-            else:
-                reach_end = True
-        p += 1
-        print((f"page: {p}, pid range: "
-               f"{max([int(x['pid']) for x in page_data])} "
-               f"———— {min([int(x['pid']) for x in page_data])}"
-               f" cost time: {time.time() - start} s"))
+        max_id, min_id = spider_one_page(page, getcomment_url,
+                                        store_path, pid_list)
+        # print((f"page: {p}, pid range: "
+        #     f"({max_id}, {min_id})"
+        #     f", cost time: {time.time() - start} s"))
         time.sleep(sleep_time)
+
+    
+        
+        
 
--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import requests
+import json
+import os
 
 
 HEADERS = {
@@ -34,3 +36,32 @@ def getlist(p, url):
     else:
         raise Exception(f"getlist action fail, response is :\n{response}")
 
+
+def spider_one_page(page, getcomment_url, folder, pid_list):
+    """对单个页面进行爬取, 并将数据存储到文件中
+
+    Args:
+        page: str, api请求后获取的文本.
+        comment_url: 获取评论api接口.
+        folder: 存储文件的目录.
+        end: 爬取帖子id最小值
+        start: 爬取帖子id最大值
+    Returns:
+        max_pid, min_pid: 该页面pid最大最小值
+    """
+    page = json.loads(page, encoding='utf-8')
+    page_data = page['data']
+    if page_data != []:
+        for post in page_data:
+            pid = post['pid']
+            if pid not in pid_list:
+                comment = getcomment(pid, getcomment_url)
+                post['comment'] = json.loads(comment, encoding='utf-8')
+                dst = os.path.join(folder, f"{pid}.json")
+                with open(dst, 'w', encoding='utf-8') as f:
+                    f.write(json.dumps(post, ensure_ascii=False))
+    
+        pid_list = [int(x['pid']) for x in page_data]
+        return max(pid_list), min(pid_list)
+    else:
+        return -1, -1
