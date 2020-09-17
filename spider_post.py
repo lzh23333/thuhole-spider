829746f1c95940f7688c65280eb72daf4c38babd
Robinlin <robinlin163@163.com>
2020-09-17 23:19:27 +0800

    完善了两种爬取模式

---
+++ spider_post.py
@@ -0,0 +1,94 @@
+"""T大树洞爬虫
+"""
+import json
+import argparse
+import os
+import time
+from tqdm import tqdm
+from utils import getcomment, getlist
+from utils import spider_one_page
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="T大树洞爬虫, 程序会自动爬取帖子以及对应评论内容")
+    parser.add_argument("--start", help="爬取帖子索引最大值，若不指定，默认为最新帖子的id",
+                        type=int)
+    parser.add_argument("--end",
+                        help="爬取帖子索引最小值，若不指定，程序会自动检测路径中索引最大的id",
+                        type=int)
+    parser.add_argument("--config", help="爬虫配置文件路径",
+                        default="./config.json")
+    parser.add_argument("--store-path", help="数据存储目录",
+                        default="./posts")
+    parser.add_argument("--sleep-factor", help="爬取一个未曾读取的帖子后休眠时间/s",
+                        type=float, default=0.1)
+    parser.add_argument("--mode",
+                        help=("爬取模式, normal对应根据start-end确定爬取范围,"
+                              "fix-missing对应根据目录，补全缺失帖子"),
+                        default="normal")
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+
+    # 加载参数
+    config_file = args.config
+    
+    with open(config_file, 'r') as f:
+        config = json.load(f)
+
+    # API
+    getone_url = (f"{config['api_url']}&action=getone&"
+                f"user_token={config['user_token']}")
+    getcomment_url = (f"{config['api_url']}&action=getcomment"
+                    f"&user_token={config['user_token']}")
+    getlist_url = (f"{config['api_url']}&action=getlist"
+                f"&user_token={config['user_token']}")
+    store_path = args.store_path
+    sleep_factor = args.sleep_factor
+
+    # 获取最新帖子pid
+    max_pid = 0
+    page = getlist(1, getlist_url)
+    page = json.loads(page, encoding='utf-8')
+    max_pid = max(map(lambda x: int(x['pid']), page['data']))
+    start_pid = max_pid if args.start is None else args.start
+    end_pid = args.end if args.end is not None else 1
+
+    # 获取已爬取的帖子
+    if os.path.exists(store_path):
+        pid_list = list(map(lambda x: int(x.split('.')[0]),
+                            os.listdir(store_path)))
+        if end_pid == 1:
+            end_pid = max(pid_list)
+    else:
+        os.makedirs(store_path)
+        pid_list = []
+    
+    # 根据start end 爬取页面
+    if args.mode == "normal":
+        start_page = (max_pid - start_pid) // 30 + 1
+        end_page = (max_pid - end_pid) // 30 + 1
+        print("spider mode: normal")
+        print(f"spider pid range: {start_pid, end_pid}")
+        print(f"spider page from {start_page} to {end_page}")
+        for p in tqdm(range(start_page, end_page)):
+            p_list = spider_one_page(p, getlist_url, store_path)
+            time.sleep(1 + len(p_list) * sleep_factor)
+
+    # 完整爬取，并忽略已爬取的帖子
+    elif args.mode == 'fix-missing':
+        page_num = max_pid // 30 + 1
+        for p in tqdm(range(p, page_num + 1)):
+            start_t = time.time()
+            page = getlist(p, getlist_url)
+            p_list = spider_one_page(page, getcomment_url,
+                                    store_path, pid_list)
+            time.sleep(1 + len(p_list) * sleep_factor)
+
+    else:
+        raise Exception(f"mode {args.mode} isn't supported")
+        
+        
+
--- utils.py
+++ utils.py
@@ -38,28 +38,30 @@ def getlist(p, url):
         raise Exception(f"getlist action fail, response is :\n{response}")
 
 
-def spider_one_page(page, getcomment_url, folder, pid_list):
-    """对单个页面进行爬取, 并将数据存储到文件中
+def spider_one_page(p, getlist_url, folder, pid_list=None):
+    """对单个页面进行爬取, 并将数据存储到文件中.
 
     Args:
-        page: str, api请求后获取的文本.
-        comment_url: 获取评论api接口.
-        folder: 存储文件的目录.
-        end: 爬取帖子id最小值
-        start: 爬取帖子id最大值
+        p (int): 获取第p页的帖子.
+        comment_url (str): 获取评论api接口.
+        folder (str): 存储文件的目录.
+        pid_list (list [int]): 已有的帖子id列表.
     Returns:
-        count: 该页中包含读取帖子的个数
+        pid_list (list [int]): 该页中包含读取帖子id列表.
     """
+    page = getlist(p, getlist_url)
     page = json.loads(page, encoding='utf-8')
     page_data = page['data']
+    if pid_list is None:
+        pid_list = []
     post_list = filter(lambda x: int(x['pid']) not in pid_list, page_data)
     post_list = list(post_list)
     def post_dump(post):
         # 包装为小线程
         pid = post['pid']
         
-        comment = getcomment(pid, getcomment_url)
-        post['comment'] = json.loads(comment, encoding='utf-8')
+        # comment = getcomment(pid, getcomment_url)
+        # post['comment'] = json.loads(comment, encoding='utf-8')
         dst = os.path.join(folder, f"{pid}.json")
         with open(dst, 'w', encoding='utf-8') as f:
             f.write(json.dumps(post, ensure_ascii=False))
@@ -67,7 +69,28 @@ def spider_one_page(page, getcomment_url, folder, pid_list):
     for post in post_list:
         t = threading.Thread(target=post_dump, args=(post, ))
         t.start()
+    return [x['pid'] for x in post_list]
+
+
+def spider_comments(pid_list, getcomment_url, folder):
+    """多线程根据id列表爬取对应的评论，并将其存入json文件.
+
+    Args:
+        pid_list (list [int]): 需要爬取的pid文件.
+        getcomment_url (str): 获取评论接口api.
+        folder (str): 存储文件目录.
+    Raises:
+        AssertError: pid_list中数目超过最大容许的数目.
+    """
+    assert len(pid_list) > 30, "pid list中的id数不得高于30"
+
+    def comment_dump(pid):
+        """ 根据pid获取单个评论内容，并保存
+        """
+        commment = getcomment(pid, getcomment_url)
+        with open(os.path.join(folder, f"c{pid}.json"), 'w') as f:
+            f.write(commment)
+    for pid in pid_list:
+        t = threading.Thread(target=comment_dump, args=(pid, ))
+        t.start()
     
-    return len(post_list)
-
-
